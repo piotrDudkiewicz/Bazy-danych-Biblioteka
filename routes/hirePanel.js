1edb4dfb7fbb93f675d4484fa9b0de25a6d23f5e
Piotr Dudkiewicz <59147082+piotrDudkiewicz@users.noreply.github.com>
2020-06-17 21:12:06 +0200

    Update hirePanel.js

--- routes/hirePanel.js
+++ routes/hirePanel.js
@@ -8,10 +8,10 @@ const allModels = require('../config/lib');
 
 router.get('/add', (req, res) => {
      res.render('addHire.ejs', {
-     page:'Wypożyczanie samochodu',
-     menuId:'addHire',
-     isLoggedIn: req.isAuthenticated()
-   });
+          page: 'Wypożyczanie samochodu',
+          menuId: 'addHire',
+          isLoggedIn: req.isAuthenticated()
+     });
 });
 
 router.post('/add', async (req, res) => {
@@ -19,8 +19,8 @@ router.post('/add', async (req, res) => {
      if (new Date(req.body.date) <= new Date() || req.body.date == null)
           return res.render('addHire.ejs', {
                message: "Popraw datę",
-               page:'Wypożyczanie samochodu',
-               menuId:'addHire',
+               page: 'Wypożyczanie samochodu',
+               menuId: 'addHire',
                isLoggedIn: req.isAuthenticated()
           });
 
@@ -42,15 +42,15 @@ router.post('/add', async (req, res) => {
                if (userTemp == null) {
                     res.render('addHire.ejs', {
                          message: "Nie ma takiego użytkownika",
-                         page:'Wypożyczanie samochodu',
-                         menuId:'addHire',
+                         page: 'Wypożyczanie samochodu',
+                         menuId: 'addHire',
                          isLoggedIn: req.isAuthenticated()
                     });
                } else if (carTemp == null) {
                     res.render('addHire.ejs', {
                          message: "Nie ma takiego samochodu",
-                         page:'Wypożyczanie samochodu',
-                         menuId:'addHire',
+                         page: 'Wypożyczanie samochodu',
+                         menuId: 'addHire',
                          isLoggedIn: req.isAuthenticated()
                     });
                } else {
@@ -62,16 +62,16 @@ router.post('/add', async (req, res) => {
                     await newHire.save();
                     res.render('addHire.ejs', {
                          message: "Dodano",
-                         page:'Wypożyczanie samochodu',
-                         menuId:'addHire',
+                         page: 'Wypożyczanie samochodu',
+                         menuId: 'addHire',
                          isLoggedIn: req.isAuthenticated()
                     });
                }
           } else {
                res.render('addHire.ejs', {
                     message: "Ten samochód jest wypożyczony.",
-                    page:'Wypożyczanie samochodu',
-                    menuId:'addHire',
+                    page: 'Wypożyczanie samochodu',
+                    menuId: 'addHire',
                     isLoggedIn: req.isAuthenticated()
                });
           }
@@ -79,8 +79,8 @@ router.post('/add', async (req, res) => {
      } catch (e) {
           res.render('addHire.ejs', {
                message: e,
-               page:'Wypożyczanie samochodu',
-               menuId:'addHire',
+               page: 'Wypożyczanie samochodu',
+               menuId: 'addHire',
                isLoggedIn: req.isAuthenticated()
           });
      }
@@ -107,8 +107,8 @@ router.get('/getAll', async (req, res) => {
                manufacturer: req.query.manufacturer,
                model: req.query.model,
                type: "service",
-               page:'Lista wypożyczeń',
-               menuId:'hireList',
+               page: 'Lista wypożyczeń',
+               menuId: 'hireList',
                isLoggedIn: req.isAuthenticated()
           });
 
@@ -120,15 +120,17 @@ router.get('/getAll', async (req, res) => {
                models: modelList,
                manufacturer: req.query.manufacturer,
                model: req.query.model,
-               page:'Lista wypożyczeń',
-               menuId:'hireList',
+               page: 'Lista wypożyczeń',
+               menuId: 'hireList',
                isLoggedIn: req.isAuthenticated()
           });
      }
 });
 
 router.get('/end/:id', async (req, res) => {
+     let manufacturerList;
      try {
+          manufacturerList = await allModels.manufacturers();
           let hireTemp = await hire.updateOne({
                _id: req.params.id
           }, {
@@ -139,17 +141,22 @@ router.get('/end/:id', async (req, res) => {
                _id: req.params.id
           });
 
+          const aggregateOption = await allModels.hiresAggregate(req);
+          const hires = await hire.aggregate(aggregateOption);
+
           res.render('getHireList.ejs', {
                message: "Do zapłaty: " + hireTemp.price,
-               page:'Lista wypożyczeń',
-               menuId:'hireList',
+               manufacturers: manufacturerList,
+               page: 'Lista wypożyczeń',
+               menuId: 'hireList',
+               hires: hires,
                isLoggedIn: req.isAuthenticated()
           });
      } catch (e) {
           res.render('getHireList.ejs', {
                message: "Coś poszło nie tak ",
-               page:'Lista wypożyczeń',
-               menuId:'hireList',
+               page: 'Lista wypożyczeń',
+               menuId: 'hireList',
                isLoggedIn: req.isAuthenticated()
           });
      }
